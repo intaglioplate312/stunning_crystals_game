687aa73e2868f865e26945085aae2d0f4a921914
intaglioplate312 <helenelizabeth@me.com>
2017-05-23 12:36:17 -0500

    psuedocode start, thinking out loud

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+
+//Thinking out loud ...
+
+//There will be four crystals displayed as buttons on the page.
+//Each crystal should have a random hidden value between 1 - 12.
+var pearl = {}
+var alexandrite = {}
+var moonstone = {}
+var emerald = {}
+var Win = 0
+var Loss = 0
+var playerChoices
+
+
+//The random number shown at the start of the game should be between 19 - 120.
+//The player will be shown a random number at the start of the game.
+var computerNum
+push.computerNum
+//When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+ ++ gem
+//Your game will hide this amount until the player clicks a crystal.
+
+//When they do click one, update the player's score counter.
+updateScore
+//The player wins if their total score matches the random number from the beginning of the game.
+(playerChoices == ComputerNum) {
+   return "You Won!";
+}
+//The player loses if their score goes above the random number.
+(playerChoices > ComputerNum) {
+   return "Sorry you lose";
+//The game restarts whenever the player wins or loses.
+
+//When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+var reset = {}
+var updatedWin = {}
+var updatedLoss = {}
+var updatedComputerNum = {}
+
+
+//The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
