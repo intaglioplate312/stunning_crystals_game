4649d0f3ad499dd8ac328f71013cd44bc96cb445
intaglioplate312 <helenelizabeth@me.com>
2017-05-24 16:18:08 -0500

    start of logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,28 @@
-
-//Thinking out loud ...
-
-//There will be four crystals displayed as buttons on the page.
+ // Handler for .ready() called ... and so it starts
+$( document ).ready(function(){
+ 
+//Start of game
+//Computer gerates random number
+var randomNum = Math.floor(Math.random()*120);
+// display number to match
+$('#computerChoice').text(randomNum);
+/*
 //Each crystal should have a random hidden value between 1 - 12.
-var pearl = {}
-var alexandrite = {}
-var moonstone = {}
-var emerald = {}
-var Win = 0
-var Loss = 0
-var playerChoices
+var computerChoice = Math.floor(Math.random()*120) + 1;
+var crystalOne = Math.floor(Math.random()*12) + 1;
+var crystalTwo = Math.floor(Math.random()*12) + 1;
+var crystalThree = Math.floor(Math.random()*12) + 1;
+var crystalFour = Math.floor(Math.random()*12) + 1;
+
+//For start no display
+var playerChoices = 0;
+var Wins = 0;
+var Losses = 0;
+
 
 
 //The random number shown at the start of the game should be between 19 - 120.
 //The player will be shown a random number at the start of the game.
-var computerNum
 push.computerNum
 //When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
  ++ gem
@@ -40,3 +48,10 @@ var updatedComputerNum = {}
 
 //The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 
+
+
+
+*/
+
+
+}); //closes document_ready_function
